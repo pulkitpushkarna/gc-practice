b7187e2e937af7aa5fcbdcab9ea5d9b0e0a580f8
Pulkit <pulkit@intelligrape.com>
2017-10-06 22:42:23 +0530

    Boostrap.java introduced

---
+++ src/main/java/com/springmvc/events/Bootstrap.java
@@ -0,0 +1,16 @@
+package com.springmvc.events;
+
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bootstrap {
+
+    @EventListener(ContextRefreshedEvent.class)
+    void startUpEvent(){
+
+
+    }
+
+}
