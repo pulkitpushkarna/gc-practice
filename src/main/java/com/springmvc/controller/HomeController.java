27c3741188e813a41ffe404463934e93b4c18cb2
Jitendra Singh <jeet.mp3@gmail.com>
2017-10-07 19:32:18 +0530

    Log 4j added

--- src/main/java/com/springmvc/controller/HomeController.java
+++ src/main/java/com/springmvc/controller/HomeController.java
@@ -1,22 +1,12 @@
 package com.springmvc.controller;
 
-import com.springmvc.co.CabRequestCO;
-import com.springmvc.entity.CabRequest;
 import com.springmvc.repositories.NewerRepository;
-import com.springmvc.service.CabRequestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.social.security.SocialUser;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
-
 @Controller
-@Secured("ROLE_NEWER")
 public class HomeController {
 
     @Autowired
